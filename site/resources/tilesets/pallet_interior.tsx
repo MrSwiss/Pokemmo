3e5f6c60986f815265454d79c93282bcf57944c2
Matheus28 <matheusavs3@gmail.com>
2012-02-12 18:42:33 -0200

    Little fix

---
+++ site/resources/tilesets/pallet_interior.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pallet_interior" tilewidth="32" tileheight="32">
+ <image source="pallet_interior.png" width="320" height="320"/>
+ <tile id="4">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="solid" value="1"/>
+  </properties>
+ </tile>
+</tileset>
