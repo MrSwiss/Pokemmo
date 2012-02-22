292f9e6e7acb34c38d5d7d51fbcede99b1ad426a
Matheus28 <matheusavs3@gmail.com>
2012-02-22 13:29:12 -0200

    Fixed it again, for some reason it's still different from the table, maybe the formula is incorrect on bulbapedia

--- server/loadExperience.js
+++ server/loadExperience.js
@@ -2,50 +2,94 @@
 // The table works as this: experienceRequired.fast[100] would be
 // the experience that a level 99 pokémon needs to acquire to reach level 100
 // (experience needed at level 99, not total)
+
+var MAX_LEVEL = 100;
 (function(){
-	var arr;
+	var arr, func;
 	
 	experienceRequired.erratic = arr = [0];
 	for(var i=1;i<=100;++i){
 		if(i <= 50){
-			arr[i] = Math.floor(i * i * i * (100 - i) / 50) - arr[i - 1];
+			arr[i] = Math.round(i * i * i * (100 - i) / 50);
 		}else if(i <= 68){
-			arr[i] = Math.floor(i * i * i * (150 - i) / 100) - arr[i - 1];
+			arr[i] = Math.round(i * i * i * (150 - i) / 100);
 		}else if(i <= 98){
-			arr[i] = Math.floor(i * i * i * ((1911 - 10 * i) / 3) / 500) - arr[i - 1];
+			arr[i] = Math.round((i * i * i * ((1911 - 10 * i) / 3)) / 500);
 		}else{
-			arr[i] = Math.floor(i * i * i * (160 - i) / 100) - arr[i - 1];
+			arr[i] = Math.round(i * i * i * (160 - i) / 100);
+		}
+	}
+	
+	{
+		var i = MAX_LEVEL + 1;
+		while(i-- > 1){
+			arr[i] -= arr[i - 1];
 		}
 	}
 	
 	experienceRequired.fast = arr = [0];
-	for(var i=1;i<=100;++i){
-		arr[i] = Math.floor(4 * i * i * i / 5) - arr[i - 1];
+	for(var i=1;i<=MAX_LEVEL;++i){
+		arr[i] = Math.round(4 * i * i * i / 5);
+	}
+	
+	{
+		var i = MAX_LEVEL + 1;
+		while(i-- > 1){
+			arr[i] -= arr[i - 1];
+		}
 	}
 	
 	experienceRequired.mediumFast = arr = [0];
-	for(var i=1;i<=100;++i){
-		arr[i] = Math.floor(i * i * i - arr[i - 1]);
+	for(var i=1;i<=MAX_LEVEL;++i){
+		arr[i] = Math.round(i * i * i);
+	}
+	
+	{
+		var i = MAX_LEVEL + 1;
+		while(i-- > 1){
+			arr[i] -= arr[i - 1];
+		}
 	}
 	
 	experienceRequired.mediumSlow = arr = [0];
-	for(var i=1;i<=100;++i){
-		arr[i] = Math.floor(6 / 5 * i * i * i - 15 * i * i + 100 * i - 140) - arr[i - 1];
+	for(var i=1;i<=MAX_LEVEL;++i){
+		arr[i] = Math.round(6 / 5 * i * i * i - 15 * i * i + 100 * i - 140);
+	}
+	
+	{
+		var i = MAX_LEVEL + 1;
+		while(i-- > 1){
+			arr[i] -= arr[i - 1];
+		}
 	}
 	
 	experienceRequired.slow = arr = [0];
-	for(var i=1;i<=100;++i){
-		arr[i] = Math.floor(5 * i * i * i / 4) - arr[i - 1];
+	for(var i=1;i<=MAX_LEVEL;++i){
+		arr[i] = Math.round(5 * i * i * i / 4);
+	}
+	
+	{
+		var i = MAX_LEVEL + 1;
+		while(i-- > 1){
+			arr[i] -= arr[i - 1];
+		}
 	}
 	
 	experienceRequired.fluctuating = arr = [0];
-	for(var i=1;i<=100;++i){
+	for(var i=1;i<=MAX_LEVEL;++i){
 		if(i <= 15){
-			arr[i] = Math.floor(i * i * i * (((i + 1) / 3 + 24) / 50)) - arr[i - 1];
+			arr[i] = Math.round(i * i * i * (((i + 1) / 3 + 24) / 50));
 		}else if(i <= 36){
-			arr[i] = Math.floor(i * i * i * ((i + 14) / 50)) - arr[i - 1];
+			arr[i] = Math.round(i * i * i * ((i + 14) / 50));
 		}else{
-			arr[i] = Math.floor(i * i * i * ((i / 2 + 32) / 50)) - arr[i - 1];
+			arr[i] = Math.round(i * i * i * ((i / 2 + 32) / 50));
+		}
+	}
+	
+	{
+		var i = MAX_LEVEL + 1;
+		while(i-- > 1){
+			arr[i] -= arr[i - 1];
 		}
 	}
 })();
