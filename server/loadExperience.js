9e1fda40ee50ac9fba80f77fc08039339298cc50
Matheus28 <matheusavs3@gmail.com>
2012-02-22 10:49:10 -0200

    Fixed loadExperience.js
    I was kinda sleepy when I coded this, sorry.

--- server/loadExperience.js
+++ server/loadExperience.js
@@ -8,54 +8,44 @@
 	experienceRequired.erratic = arr = [0];
 	for(var i=1;i<=100;++i){
 		if(i <= 50){
-			arr[i] = i * i * i * (100 - i) / 50;
-		}else if(n <= 68){
-			arr[i] = i * i * i * (150 - i) / 100;
-		}else if(n <= 98){
-			arr[i] = i * i * i * ((1911 - 10 * i) / 3) / 500
+			arr[i] = Math.floor(i * i * i * (100 - i) / 50) - arr[i - 1];
+		}else if(i <= 68){
+			arr[i] = Math.floor(i * i * i * (150 - i) / 100) - arr[i - 1];
+		}else if(i <= 98){
+			arr[i] = Math.floor(i * i * i * ((1911 - 10 * i) / 3) / 500) - arr[i - 1];
 		}else{
-			arr[i] = i * i * i * (160 - n) / 100;
+			arr[i] = Math.floor(i * i * i * (160 - i) / 100) - arr[i - 1];
 		}
 	}
 	
 	experienceRequired.fast = arr = [0];
 	for(var i=1;i<=100;++i){
-		arr[i] = 4 * i * i * i / 5 - arr[i - 1];
-		
-		arr[i] = Math.floor(arr[i]);
+		arr[i] = Math.floor(4 * i * i * i / 5) - arr[i - 1];
 	}
 	
 	experienceRequired.mediumFast = arr = [0];
 	for(var i=1;i<=100;++i){
-		arr[i] = i * i * i - arr[i - 1];
-		
-		arr[i] = Math.floor(arr[i]);
+		arr[i] = Math.floor(i * i * i - arr[i - 1]);
 	}
 	
 	experienceRequired.mediumSlow = arr = [0];
 	for(var i=1;i<=100;++i){
-		arr[i] = 6 / 5 * i * i * i - 15 * i * i + 100 * i - 140 - arr[i - 1];
-		
-		arr[i] = Math.floor(arr[i]);
+		arr[i] = Math.floor(6 / 5 * i * i * i - 15 * i * i + 100 * i - 140) - arr[i - 1];
 	}
 	
 	experienceRequired.slow = arr = [0];
 	for(var i=1;i<=100;++i){
-		arr[i] = 5 * i * i * i / 4 - arr[i - 1];
-		
-		arr[i] = Math.floor(arr[i]);
+		arr[i] = Math.floor(5 * i * i * i / 4) - arr[i - 1];
 	}
 	
 	experienceRequired.fluctuating = arr = [0];
 	for(var i=1;i<=100;++i){
-		if(n <= 15){
-			arr[i] = i * i * i * (((n + 1) / 3 + 24) / 50);
-		}else if(n <= 36){
-			arr[i] = i * i * i * ((n + 14) / 50);
+		if(i <= 15){
+			arr[i] = Math.floor(i * i * i * (((i + 1) / 3 + 24) / 50)) - arr[i - 1];
+		}else if(i <= 36){
+			arr[i] = Math.floor(i * i * i * ((i + 14) / 50)) - arr[i - 1];
 		}else{
-			arr[i] = i * i * i * ((n / 2 + 32) / 50)
+			arr[i] = Math.floor(i * i * i * ((i / 2 + 32) / 50)) - arr[i - 1];
 		}
-		
-		arr[i] = Math.floor(arr[i]);
 	}
 })();
